d3fa5d1be2546f5879e1f6f560b7905978766e72
VaibhavGupta98 <31921413+VaibhavGupta98@users.noreply.github.com>
2022-08-20 10:58:47 +0530

    Multi module project

---
+++ ChildModule1/src/main/java/configs/Child1.java
@@ -0,0 +1,11 @@
+package configs;
+
+public class Child1 {
+	
+	
+	public static String getString() {
+		return "Hello from Module 1";
+	}
+	
+
+}
---
+++ ChildModule2/src/test/java/tests/Child2Test.java
@@ -0,0 +1,21 @@
+package tests;
+
+import configs.Child1;
+
+public class Child2Test {
+
+	
+	public static void main(String ard[]) {
+		
+		
+		Child1 ch = new Child1();
+		
+		System.out.print(ch.getString());
+		
+		
+		
+	}
+	
+	
+	
+}
